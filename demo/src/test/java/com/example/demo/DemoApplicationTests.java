ef2b93dc51802fe5adca9193ea1d1741c9a3ffed
Aytac Agazade <aytacagazade1603@gmail.com>
2025-05-22 10:56:33 +0400

    QuizApp

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import com.example.demo.quiz.quizApp.DemoApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 class DemoApplicationTests {
 
 	@Test
